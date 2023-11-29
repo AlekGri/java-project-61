549507b6b3adcce7ae960bdd428376994208cf8d
Grigoreva Aleksandra <yunarichan@gmail.com>
2023-11-29 18:36:01 +0300

    add Even game

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,9 +1,29 @@
 package hexlet.code;
 
-import static hexlet.code.Cli.getName;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        getName();
+        System.out.println("Please enter the game number and press Enter.\n" +
+                "1 - Greet\n" +
+                "2 - Even\n" +
+                "0 - Exit");
+        System.out.print("Your choice: ");
+        Scanner scanner = new Scanner(System.in);
+        String startChoice = scanner.next();
+        System.out.println("");
+
+        switch (startChoice) {
+            case "0":
+                System.out.println("Exiting game now. Hope you had fun)");
+                break;
+            case "1":
+                Cli.getName();
+                break;
+            case "2":
+                Even.startEven();
+                break;
+        }
+        scanner.close();
     }
 }
---
+++ app/src/main/java/hexlet/code/Even.java
@@ -0,0 +1,57 @@
+package hexlet.code;
+
+import java.util.Scanner;
+public class Even {
+    public static final int MAX_RIGHT_ANSWERS = 3;
+    public static String userName;
+    public static void startEven() {
+        System.out.println("Welcome to the Brain Games!");
+        System.out.println("May I have your name?");
+        Scanner scanner = new Scanner(System.in);
+        userName = scanner.next();
+        System.out.println("Hello, " + userName + "!");
+
+        System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
+        int countAnswer = 0;
+        while (true) {
+            int count = getRound();
+            countAnswer += count;
+            if (countAnswer >= MAX_RIGHT_ANSWERS) {
+                System.out.println("Congratulations, " + userName  + "!");
+                break;
+            }
+        }
+        scanner.close();
+    }
+
+    public static int getRound() {
+        int count = 0;
+        int random = getRandom(1, 100);
+        System.out.println("Question: " + random);
+        System.out.print("Your answer: ");
+        Scanner scanner = new Scanner(System.in);
+        String userChoice = scanner.next();
+        boolean isRandomEven = isEven(random);
+        String correctAnswer = "no";
+        if (isRandomEven) {
+            correctAnswer = "yes";
+        }
+
+        if ((userChoice.equals("yes") && isRandomEven) || (userChoice.equals("no") && !isRandomEven)) {
+            count += 1;
+            System.out.println("Correct!");
+        } else {
+            System.out.println("'" + userChoice + "' is wrong answer ;(. Correct answer was '" + correctAnswer + "'.\n" +
+                    "Let's try again, " + userName + "!");
+        }
+        return count;
+    }
+
+    public static int getRandom (int min, int max){
+        return (int) (Math.random() * (max - min)) + min;
+    }
+
+    public static boolean isEven (int num) {
+        return num % 2 == 0;
+    }
+}
