ba78a2894b06a5db6b759d89703b3a53149214fd
Grigoreva Aleksandra <yunarichan@gmail.com>
2024-01-07 19:25:06 +0300

    change Even.java, Engine.java

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -7,6 +7,7 @@ public class Engine {
     public static String getUserName() {
         return userName;
     }
+    public static final int MAX_RIGHT_ANSWERS = 3;
     public static void greeting(String question) {
         System.out.println("Welcome to the Brain Games!");
         System.out.println("May I have your name?");
@@ -15,6 +16,25 @@ public class Engine {
         System.out.println("Hello, " + userName + "!");
 
         System.out.println(question);
+    }
+
+    public static void doRound(String[][] questionsAndAnswers) {
+        for (int i = 0; i < MAX_RIGHT_ANSWERS; i++) {
+            System.out.println("Question: " + questionsAndAnswers[i][0]);
+            System.out.print("Your answer: ");
+            Scanner scanner = new Scanner(System.in);
+            String userAnswer = scanner.next();
+
+            if (userAnswer.equals(questionsAndAnswers[i][1])) {
+                System.out.println("Correct!");
+            } else {
+                System.out.println("'" + userAnswer + "' is wrong answer ;(. " +
+                        "Correct answer was '" + questionsAndAnswers[i][1] + "'");
+                System.out.println("Let's try again, " + Engine.getUserName() + "!");
+                return;
+            }
+        }
+        System.out.println("Congratulations, " + Engine.getUserName()  + "!");
     }
 
     public static int getRandom(int min, int max) {
--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -1,16 +1,14 @@
 package hexlet.code.games;
 
-import java.util.Scanner;
 import hexlet.code.Engine;
 public class Even {
     public static final int MAX_RIGHT_ANSWERS = 3;
     public static final String QUESTION = "Answer 'yes' if the number is even, otherwise answer 'no'.";
-    private static String rightAnswer;
     public static final int MAX_RANDOM_NUMBER = 100;
-    private static String[][] questionsAndAnswers = new String[MAX_RIGHT_ANSWERS][2];
 
     public static void startEven() {
         Engine.greeting(QUESTION);
+        String[][] questionsAndAnswers = new String[MAX_RIGHT_ANSWERS][2];
 
         for (int i = 0; i < MAX_RIGHT_ANSWERS; i++) {
             int random = Engine.getRandom(1, MAX_RANDOM_NUMBER);
@@ -18,21 +16,7 @@ public class Even {
             questionsAndAnswers[i][1] = (isEven(random)) ? "yes" : "no";
         }
 
-        for (int i = 0; i < MAX_RIGHT_ANSWERS; i++) {
-            System.out.println("Question: " + questionsAndAnswers[i][0]);
-            System.out.print("Your answer: ");
-            Scanner scanner = new Scanner(System.in);
-            String userAnswer = scanner.next();
-
-            if (userAnswer.equals(questionsAndAnswers[i][1])) {
-                System.out.println("Correct!");
-            } else {
-                System.out.println("'" + userAnswer + "' is wrong answer ;(. Correct answer was '" + questionsAndAnswers[i][1] + "'");
-                System.out.println("Let's try again, " + Engine.getUserName() + "!");
-                return;
-            }
-        }
-        System.out.println("Congratulations, " + Engine.getUserName()  + "!");
+        Engine.doRound(questionsAndAnswers);
     }
 
     public static boolean isEven(int num) {
