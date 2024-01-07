8af67ca62515c30578683ad0f53cc2b4080e3368
Grigoreva Aleksandra <yunarichan@gmail.com>
2024-01-07 17:58:57 +0300

    change Even.java

--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -5,43 +5,34 @@ import hexlet.code.Engine;
 public class Even {
     public static final int MAX_RIGHT_ANSWERS = 3;
     public static final String QUESTION = "Answer 'yes' if the number is even, otherwise answer 'no'.";
-
-    private static String userAnswer;
     private static String rightAnswer;
     public static final int MAX_RANDOM_NUMBER = 100;
+    private static String[][] questionsAndAnswers = new String[MAX_RIGHT_ANSWERS][2];
 
     public static void startEven() {
         Engine.greeting(QUESTION);
 
         for (int i = 0; i < MAX_RIGHT_ANSWERS; i++) {
-            boolean roundResult = doRound();
+            int random = Engine.getRandom(1, MAX_RANDOM_NUMBER);
+            questionsAndAnswers[i][0] = String.valueOf(random);
+            questionsAndAnswers[i][1] = (isEven(random)) ? "yes" : "no";
+        }
 
-            if (roundResult) {
+        for (int i = 0; i < MAX_RIGHT_ANSWERS; i++) {
+            System.out.println("Question: " + questionsAndAnswers[i][0]);
+            System.out.print("Your answer: ");
+            Scanner scanner = new Scanner(System.in);
+            String userAnswer = scanner.next();
+
+            if (userAnswer.equals(questionsAndAnswers[i][1])) {
                 System.out.println("Correct!");
             } else {
-                System.out.println("'" + userAnswer + "' is wrong answer ;(. Correct answer was '" + rightAnswer + "'");
+                System.out.println("'" + userAnswer + "' is wrong answer ;(. Correct answer was '" + questionsAndAnswers[i][1] + "'");
                 System.out.println("Let's try again, " + Engine.getUserName() + "!");
                 return;
             }
         }
-
         System.out.println("Congratulations, " + Engine.getUserName()  + "!");
-    }
-
-    public static boolean doRound() {
-        int random = Engine.getRandom(1, MAX_RANDOM_NUMBER);
-        System.out.println("Question: " + random);
-        System.out.print("Your answer: ");
-        Scanner scanner = new Scanner(System.in);
-        userAnswer = scanner.next();
-
-        boolean isRandomEven = isEven(random);
-        rightAnswer = "no";
-        if (isRandomEven) {
-            rightAnswer = "yes";
-        }
-
-        return userAnswer.equals(rightAnswer);
     }
 
     public static boolean isEven(int num) {
