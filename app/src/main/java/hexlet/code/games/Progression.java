32c036c538c614a84bba15b131c69b87aabdf67d
Grigoreva Aleksandra <yunarichan@gmail.com>
2024-01-12 13:58:54 +0300

    change Progression.java

--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -18,36 +18,29 @@ public class Progression {
             int startNumber = Util.getRandom(MIN_START_NUMBER, MAX_START_NUMBER);
             int progressionLength = Util.getRandom(MIN_LENGTH, MAX_LENGTH);
             int step = Util.getRandom(MIN_STEP, MAX_STEP);
-            int[] progression = getProgression(startNumber, progressionLength, step);
+            String[] progression = getProgression(startNumber, progressionLength, step);
 
             int indexToHide = Util.getRandom(0, progressionLength - 1);
+            String answer = progression[indexToHide];
             String question = hideIndex(progression, indexToHide);
 
-            questionsAndAnswers[i][Engine.QUESTIONS_ROW] = String.valueOf(question);
-            questionsAndAnswers[i][Engine.ANSWERS_ROW] = String.valueOf(progression[indexToHide]);
+            questionsAndAnswers[i][Engine.QUESTIONS_ROW] = question;
+            questionsAndAnswers[i][Engine.ANSWERS_ROW] = answer;
         }
         Engine.doRound(QUESTION, questionsAndAnswers);
     }
 
-    public static int[] getProgression(int startNumber, int progressionLength, int step) {
-        int[] progression = new int[progressionLength];
+    public static String[] getProgression(int startNumber, int progressionLength, int step) {
+        String[] progression = new String[progressionLength];
         for (int i = 0; i < progressionLength; i++) {
-            progression[i] = startNumber + (i * step);
+            progression[i] = String.valueOf(startNumber + (i * step));
         }
 
         return progression;
     }
 
-    public static String hideIndex(int[] progression, int indexToHide) {
-        var question = new StringBuilder();
-        for (int i = 0; i < progression.length; i++) {
-            if (i == indexToHide) {
-                question.append(".. ");
-            } else {
-                question.append(progression[i]);
-                question.append(" ");
-            }
-        }
-        return String.valueOf(question);
+    public static String hideIndex(String[] progression, int indexToHide) {
+        progression[indexToHide] = "..";
+        return String.join(" ", progression);
     }
 }
